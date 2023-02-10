08730e573f654c6497491a8fb74c2ffc8c96b9f7
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-02-11 02:08:31 +0300

    Add files via upload

--- scripts/burgermenu.js
+++ scripts/burgermenu.js
@@ -1,10 +1,12 @@
 const toggleBtn = document.getElementById("menu-toggle-btn");
 const headerMenu = document.getElementById("header-menu");
 
+document.documentElement.clientWidth < 768 ? headerMenu.style.display = 'none' : 'flex';
+
 toggleBtn.addEventListener("click", function() {
     if (window.innerWidth < 768) {
         headerMenu.style.display = headerMenu.style.display === "none" ? "" : "none";
     }
-    
+
     toggleBtn.classList.toggle("active");
 });
